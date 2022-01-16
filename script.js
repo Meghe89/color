c75663d02921a4b30e475a6af3147e077dfb1bfd
Michele Cozzola <cozzolamichele89@gmail.com>
2022-01-16 13:26:54 +0100

    nav fix ok

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
+/* DOM LINKS */
+
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
+const aLinks = document.querySelectorAll('.navbar__item');
+const hero = document.querySelector('.hero');
 
 //DISPLAY MOBILE MENU
+//FUNCTION
+
 function mobileMenu() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active')
 }
 
+function pressLink(){
+    menuLinks.classList.remove('active')
+    menu.classList.remove('is-active');
+}
+
+/* EVENT LISTENER */
 menu.addEventListener('click', mobileMenu);
+
+aLinks.forEach(el =>{
+    el.addEventListener('click', pressLink);
+})
